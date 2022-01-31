6fd898fdb68f810b9c1d27946b2f15755bafe6d8
sei-kiu <sei.kiu.sei.kiu@gmail.com>
2022-01-31 08:55:31 +0800

    working code

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+// Light Switch
+int sensorpin = A6; // Analog input pin that the sensor is attached to
+int ledPin = 4;    // LED port
+int sensorValue = 0;        // value read from the port
+int outputValue = 0;        // value output to the PWM (analog out)
+ 
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(sensorpin, INPUT);
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(sensorpin);
+ 
+  Serial.println(sensorValue);
+ 
+  if (sensorValue < 200) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+ 
+  delay(200);
+}
